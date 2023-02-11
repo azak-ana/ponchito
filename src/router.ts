01a94a5d9759a2b33074657081752ccc34aecdd9
azak-ana <victorprincipe1@hotmail.com>
2023-02-11 14:08:19 -0200

    api rest exp

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,23 @@
 // Importa componentes do express
 import { Router } from 'express';
-import testeController from './controllers/testeController';
+import TesteController from './controllers/TesteController';
 import ValidaTeste1 from './middlewares/validaTeste1';
 // Importa TesteController
 // Instancia roteador
 const Roteador = Router();// Define rota tipo get que, para funcionar, deve ser requisitada conforme exemplo.
 // Exemplo de requisição: localhost:4000/teste/123?num=456// Onde 123 e 456 podem ser substituídos por quaisquer valores
 Roteador.get(    // URL com parêmetro :id    
-    '/teste/:id', 
-    ValidaTeste1,   // Aciona função do TesteController    
-    new testeController().teste1);
+    '/teste/:id',
+    TesteController
+    );
 export default Roteador;
+
+
+
+// REST
+
+// GET = BUSCAR = QUERY E ROUTE
+// POST = CRIAR = BODY
+// PUT = UPDATE = ROUTE/BODY
+// PATCH = UPDATE = ROUTE/BODY
+// DELETE = DELETAR = ROUTE
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 // importa o express
 import express from 'express';
+import Roteador from './router';
 // instancia o express
 const app = express();
 //configura porta e funçao executada na ativação
+app.use(Roteador);
 app.listen(4000, ()=>{console.log("Servidor Iniciado")})
 
